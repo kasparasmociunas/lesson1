19a4f3f5f8f9c368096c55f229dcb7856bd1cf20
Kasparas <kasparas.mociunas@gmail.com>
2020-09-07 20:05:37 +0300

    test
    added a class to check if number is primary

---
+++ src/app/App.java
@@ -0,0 +1,45 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//arPirminis
+		
+		
+
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please provide an Integer");
+		
+		int number = scanner.nextInt();
+		
+		 boolean isPrimary = isPrimary(number);
+		
+		System.out.println(isPrimary(number));
+		
+		
+		
+	}
+
+	private static boolean  isPrimary(int number) {
+		
+		boolean isThereRemainder = true;
+		int divider = 2;
+		
+		while (isThereRemainder && divider < number) {
+			
+			isThereRemainder = (number % divider !=0);
+			
+			divider++;
+		}
+		return isThereRemainder;
+	}
+	
+}
